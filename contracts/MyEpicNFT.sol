75f9c58ccad9f9e70151d0f06f97fe870f45cf87
Andrey Minin <ewkjf@mail.ru>
2022-08-29 12:53:45 +0400

    generating random 3 words string

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -14,22 +14,67 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
+    // So, we make a baseSvg variable here that all our NFTs can use.
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] firstWords = ["Automatic", "Huge", "Kindhearted", "Nice", "Dark", "Gusty", "Gleaming", "Alluring", "Royal", "Efficient", "Gentle", "Material", "Lopsided", "Utter", "Second-hand"];
+    string[] secondWords = ["Control", "Football", "Theory", "Fishing", "Quality", "Cell", "Power", "User", "Computer", "Hat", "Disaster", "Possibility", "Area", "Priority", "Thanks"];
+    string[] thirdWords = ["Elect", "Price", "Load", "Enhance", "Occur", "Enforce", "Realize", "Reach", "Shrug", "Rest", "Receive", "Motivate", "Hesitate", "Supplement", "Peer"];
+
     // We need to pass the name of our NFTs token and its symbol.
     constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("Saying hello from my second Solidity smart-contract!");
     }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
+    // I create a function to randomly pick a word from each array.
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        // I seed the random generator. More on this in the lesson.
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
 
     // A function our user will hit to get their NFT.
     function makeAnEpicNFT() public {
         // Get the current tokenId, this starts at 0.
         uint256 newItemId = _tokenIds.current();
 
+        // We go and randomly grab one word from each of the three arrays.
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+
+        // I concatenate it all together, and then close the <text> and <svg> tags.
+        string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+        console.log("\n--------------------");
+        console.log(finalSvg);
+        console.log("--------------------\n");
+
         // Actually mint the NFT to the sender using msg.sender.
         // msg.sender is a variable Solidity itself provides that easily gives us access to the public address of the person calling the contract.
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/4B8L");
+        _setTokenURI(newItemId, "blah");
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
         // Increment the counter for when the next NFT is minted.
